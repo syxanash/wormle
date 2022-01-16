ddc6f53dc1c33689d0fe39cc96e0a39fc904faaf
syx <si.marzulli@gmail.com>
2022-01-16 18:14:38 +0000

    fixed issue with allowed chars

--- wormle.rb
+++ wormle.rb
@@ -7,46 +7,45 @@ require 'pp'
 words_output = File.read('wordle_official_wordlist.txt')
 words_list = words_output.split("\n")
 
-File.write('./temp_words.txt', words_list)
-
 words_filtered = words_list
   .select { |word| word.size == 5 }
-  # .select { |word| word.chars.last != 's' }
   .select { |word| word.chars.first != word.chars.first.upcase }
-
-File.write('./temp_words.txt', words_filtered)
 
 loop do
   puts 'would you like me to choose the word for you?'
   answer = gets.chomp
 
   if answer == 'yes'
-    chosen = words_filtered.shuffle.first
+    choosen_word = words_filtered.shuffle.first
   else
     puts 'tell me your word:'
-    chosen = gets.chomp
+    choosen_word = gets.chomp
   end
 
-  puts "the chosen word is #{chosen}"
+  puts "the chosen word is #{choosen_word}"
   puts "what color series did you get? [B]lack/[G]reen/[Y]ello"
   answer_input = gets.chomp
   answer_sequence = answer_input.split('')
 
   if answer_input == 'ggggg'
-    puts "the word was #{chosen}!!!"
+    puts "the word was #{choosen_word}!!!"
     break
   end
 
   allowed_chars = []
 
   answer_sequence.each_with_index do |box, i|
-    temp_char = chosen[i]
+    if box == 'g' || box == 'y'
+      allowed_chars.push(choosen_word[i])
+    end
+  end
+
+  answer_sequence.each_with_index do |box, i|
+    temp_char = choosen_word[i]
     words_to_delete = []
 
     if box == 'g'
       puts "keep the #{temp_char} in position #{i}"
-
-      allowed_chars.push(temp_char)
 
       words_filtered.each do |temp_word|
         if temp_word[i] != temp_char
@@ -55,8 +54,6 @@ loop do
       end
     elsif box == 'y'
       puts "ignore #{temp_char} from position #{i}"
-
-      allowed_chars.push(temp_char)
 
       words_filtered.each do |temp_word|
         if !temp_word.include? temp_char
@@ -69,9 +66,6 @@ loop do
       end
     elsif box == 'b'
       puts "delete all words with #{temp_char}"
-
-      # "AREAE" IS ANOTHER EDGE CASE BECAUSE THE SECOND A IS GREEN
-      # BUT THE FIRST ONE IS BLACK
 
       words_filtered.each do |temp_word|
         if (temp_word.include? temp_char) && (!allowed_chars.include? temp_char)
